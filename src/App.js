bc4ebcda7df9f122386689c57bad2a29176ed772
Biruk-Tassew <biruktassew21@gmail.com>
2023-01-31 02:48:37 -0500

    feat(frontend): configured redux store and users page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import { Router, BrowserRouter as Router, Swtich } from "react-router-dom";
-
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import React from "react";
+import { Users } from "./features/users/User";
 
 function App() {
   return (
-    <h1>HI</h1>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/">
+            <Users />
+          </Route>
+          <Route path="/add-user">
+            <h1>Add user</h1>
+          </Route>
+          <Route path="/edit-user">
+            <h1>Edit user</h1>
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
